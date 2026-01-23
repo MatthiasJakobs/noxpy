a5f28553c8925dbc46cd4b80b602f94b49c6233a
Matthias Jakobs <m.jakobs265@gmail.com>
2026-01-23 22:42:24 +0100

    made things more performant by switching to numpy

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # nox-re
 Reverse-engineering the Noxmedical file formats with python.
 Based on previous work by jussivirkkala [https://github.com/jussivirkkala/Noxturnal-NDF]
+
+# todo
+Adult Bruxism 1/audio.ndf
+Bruxism T3/audio.ndf
+3.00029235853316 3.0
+Healthy Child/spo2 b-b.ndf
+Healthy Child/z axis.ndf
+75.007308963329 75.0
+Healthy Child/plethysmograph.ndf
--- nox.py
+++ nox.py
@@ -3,6 +3,9 @@ import xml.etree.ElementTree as ET
 import datetime
 import struct
 
+def read_uint8(f):
+    return struct.unpack('<B', f.read(1))[0]
+
 def read_uint16(f):
     return struct.unpack('<H', f.read(2))[0]
 
@@ -17,6 +20,23 @@ def read_int32(f):
 
 def read_double(f):
     return struct.unpack('<d', f.read(8))[0]
+
+# TODO: No idea if this is correct
+def mu2lin(mu_bytes):
+    mu_bytes = np.asarray(mu_bytes, dtype=np.uint8)
+
+    u = np.bitwise_not(mu_bytes)
+
+    sign = u & 0x80
+    exponent = (u >> 4) & 0x07
+    mantissa = u & 0x0F
+
+    magnitude = ((mantissa << 3) + 0x84) << exponent
+    pcm = magnitude - 0x84
+
+    pcm = np.where(sign != 0, -pcm, pcm)
+
+    return pcm.astype(np.int16)
 
 def header_xml_to_dict(header_xml):
 
@@ -98,6 +118,8 @@ def parse_ndf(path, verbose=False):
                     start_time = datetime.datetime.strptime(d, '%Y%m%dT%H%M%S')
                 else:
                     start_time = datetime.datetime.strptime(d, '%Y%m%dT%H%M%S.%f')
+                # This is the intended way as np.datetime64 cannot take a format string
+                start_time = np.datetime64(start_time, 'ns')
                 channel['start'].append(start_time)
                 if len(channel['start']) > 1:
                     channel['gap'].append((channel['start'][-1] - channel['end'][-1]))
@@ -106,27 +128,30 @@ def parse_ndf(path, verbose=False):
                 raise NotImplementedError('144')
             elif typ == 514:
                 # Sampling rate again?
-                # TODO: What is this?
                 sampling_rate = read_double(f)
-                if 'sampling_rate' in channel:
-                    if abs(channel['sampling_rate']-sampling_rate) > 1e-5:
-                        print(channel['sampling_rate'], sampling_rate)
-                        raise RuntimeError('Sampling rate changed')
-                else:
-                    channel['sampling_rate'] = sampling_rate
+                channel['sampling_rate'] = sampling_rate
+                # TODO: I'm pretty sure the sampling rate is in Hz anyway.
+                #       Maybe it makes more sense to round to an integer.
+                #       That way they would not change over time
+
             elif typ == 513:
                 # Data
-                if channel['header']['format'] == 'Int16':
-                    raw = [read_int16(f) for _ in range(length//2)]
+                fmt = channel['header']['format']
+                if fmt == 'Int16':
+                    raw = np.frombuffer(f.read(length), dtype=np.int16)
+                    channel['data'].append(channel['scale']*np.array(raw)+channel['offset'])
+                elif fmt == 'Byte':
+                    raw = np.frombuffer(f.read(length), dtype=np.uint8)
+                    raw = mu2lin(raw)
                     channel['data'].append(channel['scale']*np.array(raw)+channel['offset'])
                 else:
-                    raise NotImplementedError()
+                    raise NotImplementedError('Unknown format', channel['header']['format'])
 
                 if length > 0:
-                    offsets_seconds = np.arange(length//2) / channel['sampling_rate']
-                    offsets_delta = [datetime.timedelta(seconds=offset) for offset in offsets_seconds]
-                    new_t = [channel['start'][-1] + offset for offset in offsets_delta]
-                    channel['t'].extend(new_t)
+                    start = np.datetime64(channel['start'][-1], 'ns')
+                    offset_ns = (np.arange(length // 2, dtype=np.int64)*1000000000) // channel['sampling_rate']
+                    new_t = start + offset_ns.astype('timedelta64[ns]')
+                    channel['t'].append(new_t)
                     channel['end'].append(new_t[-1])
             else:
                 raise NotImplementedError('Unknown code', typ, 'with length', length)
@@ -136,10 +161,20 @@ def parse_ndf(path, verbose=False):
 
 def main():
     import glob
+    import tqdm
     ndfs = list(glob.glob('**/*.ndf'))
-    for ndf_file in ndfs:
-        print(ndf_file)
-        parse_ndf(ndf_file)
+    n = len(ndfs)
+    print(n)
+    correct = 0
+    for ndf_file in tqdm.tqdm(ndfs):
+        try:
+            parse_ndf(ndf_file)
+            correct += 1
+        except:
+            print(ndf_file)
+    print(correct, n)
+
+
 
 if __name__ == '__main__':
     main()
